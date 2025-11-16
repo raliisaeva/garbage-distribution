976c2c1b8a6453cf85ef26600e3d06b478be60a2
Ralitsa Isaeva <raliisaeva@gmail.com>
2025-11-16 15:23:46 +0200

    Update main_working.py for url in streamlit

--- main_working.py
+++ main_working.py
@@ -138,19 +138,21 @@ else:
 
 #Prompt the user to enter an url
 while True:
+	url = ""
 	with st.form(key = "url_form"):
 		url = st.text_input("Enter image URL")
 		st.form_submit_button("Submit")
 	
-    if url.lower() == "exit" or url == "":
+	if url.lower() == "exit":
         break
     try:
+		st.write("Current URL: ", url)
         response = requests.get(url)
         img = Image.open(BytesIO(response.content)).convert("L")
         img = img.resize((128, 128))
-        plt.imshow(img, cmap = 'gray')
-        plt.axis('off')
-        plt.show()
+        #plt.imshow(img, cmap = 'gray')
+        #plt.axis('off')
+        #plt.show()
 
         img_tensor = torch.tensor(np.array(img), dtype = torch.float32).unsqueeze(0).unsqueeze(0) / 255.0
         img_tensor = img_tensor.to(device)
